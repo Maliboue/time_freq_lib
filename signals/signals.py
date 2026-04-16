19f42919d11d13db09e9dc07f8c8a703afb2e944
Maliboue <vchemsol@gmail.com>
2026-04-16 11:16:54 +0200

    Rings in CWT and beating oscillation

--- signals/signals.py
+++ signals/signals.py
@@ -36,3 +36,35 @@ def cosine_with_arctan_freq(duration,
     freq_func = f0 + atan_amp * np.arctan(atan_rate*(t - t.max()//2)/2)
     s = np.cos(2*np.pi*np.cumsum(freq_func)*(1/sampling_frequency))
     return t, freq_func, s
+
+def sum_of_cosines(duration,
+                   frequency1,
+                   frequency2,
+                   sampling_frequency,
+                   amplitude1=1.0,
+                   amplitude2=1.0,
+                   phase1=0.0,
+                   phase2=0.0):
+    """
+    Sum of two cosines.
+
+    Parameters:
+        duration (float): Signal duration in seconds
+        frequency1 (float): First frequency in Hz
+        frequency2 (float): Second frequency in Hz
+        sampling_frequency (float): Sampling frequency in Hz
+        amplitude1 (float): Amplitude of first cosine (default=1.0)
+        amplitude2 (float): Amplitude of second cosine (default=1.0)
+        phase1 (float): Phase of first cosine in radians (default=0.0)
+        phase2 (float): Phase of second cosine in radians (default=0.0)
+
+    Returns:
+        t (numpy.ndarray): Time vector
+        x (numpy.ndarray): sigmal vector
+    """
+    t = np.arange(0, duration, 1 / sampling_frequency)
+
+    x1 = amplitude1 * np.cos(2 * np.pi * frequency1 * t + phase1)
+    x2 = amplitude2 * np.cos(2 * np.pi * frequency2 * t + phase2)
+    
+    return t, x1 + x2
